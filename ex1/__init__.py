afa26b0ac04d4bc80c1b6d29f02678d767feddff
Rogov <Vavner25@gmail.com>
2022-02-01 17:44:43 +0300

    Task 1 Completed

--- ex1/__init__.py
+++ ex1/__init__.py
@@ -4,8 +4,12 @@ def build_roles_tree(mapping):
     :return: дерево ролей
     """
 
+    result = {'categories': []}
+    for categoryId in mapping['categoryIdsSorted']:
+        roles = []
+        for roleId in mapping['categories'][categoryId]['roleIds']:
+            roles.append({'id': roleId, "text": mapping['roles'][roleId]['name']})
+        result['categories'].append({'id': f'category-{categoryId}', "text": mapping['categories'][categoryId]['name'],
+                                     'items': roles})
 
-    for categoryID in mapping['categoryIdsSorted']:
-        print(categoryID)
-        print(mapping)
-    # Comment
+    return result
